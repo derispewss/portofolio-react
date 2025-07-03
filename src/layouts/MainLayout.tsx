850e76041ed4e36cf93feb53ce8dd3d36a45570d
derispewss <derisfirmansyah177@gmail.com>
2025-07-03 12:21:48 +0700

    feat: init shadcn/ui

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,17 +1,10 @@
-import { Outlet, NavLink } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import Navbar from '@/components/Navbar';
 
 export default function MainLayout() {
   return (
     <div>
-      <header className="p-4 shadow flex justify-between">
-        <h1 className="text-sky-500 font-bold">Deris</h1>
-        <nav className="space-x-4">
-          <NavLink to="/" end className={({ isActive }) => isActive ? 'text-sky-500' : ''}>Home</NavLink>
-          <NavLink to="/about" className={({ isActive }) => isActive ? 'text-sky-500' : ''}>About</NavLink>
-          <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-sky-500' : ''}>Projects</NavLink>
-          <NavLink to="/contact" className={({ isActive }) => isActive ? 'text-sky-500' : ''}>Contact</NavLink>
-        </nav>
-      </header>
+      <Navbar />
       <main className="p-4">
         <Outlet />
       </main>
